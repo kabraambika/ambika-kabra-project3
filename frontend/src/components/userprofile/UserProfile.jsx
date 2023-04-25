3cd3b3c80d507bc491779661330570ba357cce00
Kabra, Ambika <kabra.am@northeastern.edu>
2023-04-24 23:43:40 -0700

    added edit icon

--- frontend/src/components/userprofile/UserProfile.jsx
+++ frontend/src/components/userprofile/UserProfile.jsx
@@ -6,7 +6,7 @@ import { Container } from "react-bootstrap";
 import { AppContext } from "../../App";
 import './UserProfile.css'
 import NewTweet from "../timeline/feed/tweet/NewTweet";
-import { DoneOutline } from "@mui/icons-material";
+import { DoneOutline, EditOutlined } from "@mui/icons-material";
 // UserProfile component is used for showing profile of users
 function UserProfile() {
     const pathParameters = useParams();
@@ -15,6 +15,7 @@ function UserProfile() {
     const[currentUserDetails, setCurrentUserDetails] = useState([]);
     const {postDB, setPostDB, activeUsername } = useContext(AppContext)
     const [optionalDesc, setOptionalDesc] = useState('')
+    const [editBio, setEditBio] = useState(false)
     useEffect(() => {
         async function getUserDetails() {
             const userResponse = await axios.get('/api/users/'+currentUsername);
@@ -35,7 +36,7 @@ function UserProfile() {
         getPostDetails();
         getUserDetails();
        
-    },[postDB, currentUsername]);
+    },[postDB, currentUsername, editBio]);
 
     const handleDeletePost = async (idToDelete) => {
         const deletePostResponse = await axios.post('/api/posts/delete/'+idToDelete);
@@ -49,6 +50,7 @@ function UserProfile() {
 
     const saveBio = async () => {
         const bioUpdateResponse = await axios.post('/api/users/edit/'+currentUsername, {bio: optionalDesc});
+        setEditBio(false);
     }
 
     return (
@@ -58,12 +60,18 @@ function UserProfile() {
                     {activeUsername === currentUsername && <NewTweet/>}
                         <div className="user-profile-header">
                             <h1 className="username-header-text">{currentUsername}</h1>
-                            {activeUsername === currentUsername &&
+                            <div>
+                                <div className="bio-label">
+                                    <span className="padding-right-15px">Optional Description</span>
+                                    { activeUsername === currentUsername && <EditOutlined className="bio-desc-icon-btn" fontSize="small" onClick={() => setEditBio(true)}/>}
+                                </div>
+                                { !editBio && <div className="bio-text-container">{currentUserDetails.bio}</div> }
+                            </div>
+                            {(activeUsername === currentUsername && editBio) &&
                             <div className="optional-desc-container">
                                 <form className="bio-desc-form">
                                     <div className="bio-desc-form-field">   
                                         <div className="bio-desc-form-input">
-                                            <label className="bio-label" htmlFor="optionalDescInput">Optional Description</label>
                                             <input 
                                             type="text" 
                                             placeholder="Write about yourself ..." 
@@ -82,8 +90,8 @@ function UserProfile() {
                             <span className="join-date-text">Joined on: {currentUserDetails.timestamp}</span>
                         </div>
                         {
-                                postsList.length == 0 && <div className="no-post-text">No posts to show</div>
-                            }
+                            postsList.length == 0 && <div className="no-post-text">No posts to show</div>
+                        }
                         <div className="my-posts-container">
                             { 
                             postsList.map((post) => {   
